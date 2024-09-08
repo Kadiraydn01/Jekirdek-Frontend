c70482d6f2696a36a00b427b483ee7b7291b97b1
Kadir Aydın <kadiraydn01@gmail.com>
2024-09-09 01:29:21 +0300

    Installation react and dependencies

--- README.md
+++
@@ -1,1 +0,0 @@
-# Jekirdek-Frontend
---
+++ crm-frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl flex justify-center items-center h-screen just font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
